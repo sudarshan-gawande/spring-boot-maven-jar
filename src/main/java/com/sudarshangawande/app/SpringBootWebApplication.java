d6a499ccb76b8a8e588adf05508761182f33d97b
sudarshangawande98 <sddfdsf>
2025-03-16 11:55:01 +0530

    first commit

---
+++ src/main/java/com/sudarshangawande/app/SpringBootWebApplication.java
@@ -0,0 +1,22 @@
+package com.sudarshangawande.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
