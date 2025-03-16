f463efa1baf47ca795097aa4cc76d6b043d81562
sudarshangawande98 <sddfdsf>
2025-03-16 18:59:21 +0530

    change in folder structure

--- src/main/java/com/sudarshangawande/app/HomeController.java
+++ src/main/java/com/sudarshangawande/app/HomeController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("name", "Sudarshan Gawande");
-        return "index";
+        model.addAttribute("email", "sudarshangawande98@gmail.com");
+        return "index"; // Loads index.html from src/main/resources/templates/
     }
 }
--- src/main/java/com/sudarshangawande/app/SpringBootWebApplication.java
+++ src/main/java/com/sudarshangawande/app/SpringBootWebApplication.java
@@ -2,21 +2,14 @@ package com.sudarshangawande.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
-public class SpringBootWebApplication {
+public class SpringBootWebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
-    }
-
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
     }
 }
